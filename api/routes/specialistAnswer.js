58a07c734da08a563b11a35ba91e9d091ed31807
Rahat87942R <rumirahat63@gmail.com>
2025-05-16 02:01:54 +0600

    Add room in specialist

--- api/routes/specialistAnswer.js
+++ api/routes/specialistAnswer.js
@@ -4,9 +4,18 @@ const twilio = require("twilio");
 
 router.post("/", (req, res) => {
   const room = req.query.room;
+
+  console.log("✅ /specialist-answer hit. Room:", room);
+
   const twiml = new twilio.twiml.VoiceResponse();
-  twiml.say("We have a customer for you. Joining the conference now.");
-  twiml.dial().conference(room);
+
+  twiml.say("We have a customer for you. Please hold while we connect you.");
+  twiml.dial().conference({
+    startConferenceOnEnter: true,
+    endConferenceOnExit: true
+  }, room);
+
+  console.log("📤 Responding with specialist join TwiML:", twiml.toString());
 
   res.type("text/xml").send(twiml.toString());
 });
