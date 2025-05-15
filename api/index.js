abec5aba0a8789a885fa9e71cffc55b5d0e48b76
Rahat87942R <rumirahat63@gmail.com>
2025-05-15 22:45:23 +0600

    Changes in folder structure

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const initiate = require("../routes/initiate");
-const specialistAnswer = require("../routes/specialistAnswer");
-const statusCallback = require("../routes/statusCallback");
-const customerConference = require("../routes/customerConference");
+const initiate = require("./routes/initiate");
+const specialistAnswer = require("./routes/specialistAnswer");
+const statusCallback = require("./routes/statusCallback");
+const customerConference = require("./routes/customerConference");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
