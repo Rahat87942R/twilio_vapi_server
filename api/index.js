d81fc6410da7b3a2e6a3a3d4e85326c8daf3ecb3
Rahat87942R <rumirahat63@gmail.com>
2025-05-15 23:21:02 +0600

    Endpoint not pushed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const initiate = require("./routes/initiate");
 const specialistAnswer = require("./routes/specialistAnswer");
+const twilMlDial = require("./routes/twiml-dial");
 const statusCallback = require("./routes/statusCallback");
 const customerConference = require("./routes/customerConference");
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/initiate", initiate);
+app.use("/twiml-dial", twilMlDial);
 app.use("/specialist-answer", specialistAnswer);
 app.use("/status-callback", statusCallback);
 app.use("/customer-conference", customerConference);
