cb170ac08872943a83e5219af12d86a23cc4b1d0
Rahat87942R <rumirahat63@gmail.com>
2025-05-16 12:55:36 +0600

    Fixed router problem

--- api/routes/specialistAnswer.js
+++ api/routes/specialistAnswer.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const router = express.Router();
 const client = require("../lib/twilioClient");
 const store = require("../utils/store");
 
@@ -28,3 +30,5 @@ router.post("/", async (req, res) => {
 
   res.sendStatus(200);
 });
+
+module.exports = router;
